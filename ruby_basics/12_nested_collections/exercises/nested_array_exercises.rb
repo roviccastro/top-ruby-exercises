88c22643f2b8aba2e0aef032b576d1b320dc016c
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-03-09 23:13:30 +0800

    Fix: Solve test 12
    - I spent a LONG and HARD time at the two tests

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -11,16 +11,26 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 
   # NOTE: if one of the nested arrays is changed, the others should **not**
   # change with it
+
+  Array.new(number_of_rows) {Array.new(seats_per_row)}
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+
+  chart[row_index].push(seat_to_add)
+
+  chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+
+  chart.push(row_to_add)
+
+  chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
@@ -28,17 +38,37 @@ def delete_seat_from_row(chart, row_index, seat_index)
   # the chart, then return the chart
 
   # Hint: explore the ruby docs to find a method for deleting from an array!
+
+  chart[row_index].delete_at(seat_index)
+
+  chart
 end
 
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+
+  chart.delete_at(row_index)
+
+  chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
 
   # NOTE: `chart` should **not** be mutated
+
+  x = (chart.reduce([]) do |total, row|
+    
+    row.select do |seat|
+      total.push(seat) if seat == nil
+    end
+
+    total
+    
+  end)
+
+  x.length
 end
 
 def find_favorite(array_of_hash_objects)
@@ -55,4 +85,32 @@ def find_favorite(array_of_hash_objects)
 
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
+
+  # array_of_hash_objects.each do |lang|
+  #   if lang[:is_my_favorite?] == false
+  #     array_of_hash_objects.delete(lang)
+  #   end
+  # end
+
+  # return array_of_hash_objects[0]
+
+  # array_of_hash_objects.all? do |language|
+  #   language[:is_my_favorite?] == false
+  #   return nil
+  # end
+
+  if array_of_hash_objects.all? {|language| language[:is_my_favorite?] == false}
+    return nil
+  else
+    array_of_hash_objects.each do |lang|
+      if lang[:is_my_favorite?] == false
+        array_of_hash_objects.delete(lang)
+      end
+    end
+
+    return array_of_hash_objects[0]
+  end
+
 end
+
+# rspec spec/nested_array_exercises_spec.rb
